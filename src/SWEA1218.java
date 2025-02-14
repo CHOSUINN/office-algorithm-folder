f167f430c4343aaee1424cc66a04965eac69b96c
juice-in <65757661+whtndls@users.noreply.github.com>
2025-02-14 10:03:19 +0900

    괄호 짝짓기

--- src/SWEA1218.java
+++ src/SWEA1218.java
@@ -1,52 +1,43 @@
-//import java.util.Scanner;
-//import java.util.Stack;
-//
-//public class SWEA1218 {
-//    public static void main(String[] args) {
-//
-//        Scanner sc = new Scanner(System.in);
-//
-//        Stack<String> stack = new Stack<>();
-//
-//        for (int t = 0; t < 10; t++) {
-//
-//            int n = Integer.parseInt(sc.nextLine());
-//            String[] arr = new String[n];
-//
-//            for (int i = 0; i < n; i++) {
-//                arr[i] = sc.next();
-//            }
-//            String answer = "";
-//
-//            for (int i = 0; i < arr.length; i++) {
-//                if (arr[i].equals("(") || arr[i].equals("[") || arr[i].equals("{") || arr[i].equals("<")) {
-//                    stack.push(arr[i]);
-//                } else if (arr[i].equals("(") && stack.peek().)
-//            }
-//
-//            for (char c : input) {
-//                if (c == '(' || c == '[' || c == '{' || c == '<') {
-//                    stack.push(c);
-//                } else if (c == ')' && stack.peek() != '(') {
-//                    break;
-//                } else if (c == ']' && stack.peek() != '[') {
-//                    break;
-//                } else if (c == '}' && stack.peek() != '{') {
-//                    break;
-//                } else if (c == '<' && stack.peek() != '>') {
-//                    break;
-//                } else if (!(stack.isEmpty())) {
-//                    stack.pop();
-//                }
-//            }
-//
-//            if (stack.isEmpty())
-//                answer += "1";
-//            else
-//                answer += "0";
-//
-//            System.out.printf("#%d %s\n", t + 1, answer);
-//        }
-//
-//    }
-//}
+import java.util.Scanner;
+import java.util.Stack;
+
+// 괄호짝짓기
+public class SWEA1218 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int t = 0; t < 10; t++) {
+
+            int n = Integer.parseInt(sc.nextLine());
+            char[] arr = sc.nextLine().toCharArray();
+
+            String answer = "";
+
+            boolean flag = false;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == ('(') || arr[i] == ('[') || arr[i] == ('{') || arr[i] == ('<')) {
+                    stack.push(arr[i]);
+                } else if ((arr[i] == (')') && stack.peek() != '(')
+                        || (arr[i] == (']') && (stack.peek() != ('[')))
+                        || (arr[i] == ('}') && (stack.peek() != ('{')))
+                        || (arr[i] == ('>') && (stack.peek() != ('<')))) {
+                    flag = true;
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+
+            if (flag)
+                answer += "0";
+            else
+                answer += "1";
+
+            System.out.printf("#%d %s\n", t + 1, answer);
+        }
+
+    }
+}
