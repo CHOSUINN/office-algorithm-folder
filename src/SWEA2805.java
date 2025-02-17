d969346c15fa1e282b29ddcf4de79ea3c4c04b37
suin <65757661+whtndls@users.noreply.github.com>
2025-02-17 18:02:44 +0900

    update

---
+++ src/SWEA2805.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// 농작물 수확하기
+public class SWEA2805 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int tc = Integer.parseInt(sc.nextLine());
+        for (int t = 0; t < tc; t++) {
+
+            int n = Integer.parseInt(sc.nextLine());
+            int[][] arr = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                String[] input = sc.nextLine().split("");
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(input[j]);
+                }
+
+            }
+
+            int answer = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j =)
+            }
+        }
+    }
+}
