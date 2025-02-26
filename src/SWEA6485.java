cd03630f91e5b4cf4bda161e70a422f6e399f67a
suin <65757661+whtndls@users.noreply.github.com>
2025-02-26 17:58:43 +0900

    update

---
+++ src/SWEA6485.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// 삼성시의 버스 노선
+public class SWEA6485 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        StringBuilder sb = new StringBuilder();
+
+        int tc = Integer.parseInt(st.nextToken());
+        for (int t = 0; t < tc; t++) {
+            sb.append("#").append(t + 1);
+
+        }
+
+
+    }
+}
