86419ce8cbb11db03fd073ea59c624aab0ae9ec7
suin <65757661+whtndls@users.noreply.github.com>
2025-02-28 17:35:53 +0900

    update

--- src/SWEA1949.java
+++ src/SWEA1949.java
@@ -22,7 +22,7 @@ public class SWEA1949 {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     static int[][] mountain;
-    static int n;
+    static int n, k;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -34,7 +34,7 @@ public class SWEA1949 {
             sb.append("#").append(t + 1);
             st = new StringTokenizer(sc.nextLine());
             n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
             mountain = new int[n][n];
 
             int maxHeight = Integer.MIN_VALUE;
@@ -60,8 +60,6 @@ public class SWEA1949 {
             int maxRoad = Integer.MIN_VALUE;
             for (int i = 0; i < height.size(); i++) {
                 int num = bfs(height.get(i));
-                if (maxRoad < num)
-                    maxRoad = num;
                 maxRoad = Math.max(num, maxRoad);
             }
 
@@ -79,27 +77,28 @@ public class SWEA1949 {
 
         while (!q.isEmpty()) {
             Node temp = q.poll();
+
+            max = Math.max(max, temp.depth);
+
             for (int delta = 0; delta < 4; delta++) {
                 int r = temp.x + dx[delta];
                 int c = temp.y + dy[delta];
-                int depth = temp.depth;
-                int k = temp.k;
+
                 if (r < 0 || r >= n || c < 0 || c >= n || temp.visited[r][c]) continue;
 
+                // 갈 수 있는 경우
+                // 본인보다 작을떄,
+                // 본인보다 높은데, 깎아서 갈 수 있을때
+
+                // 깎지 않고 지나가는 경우도 계산해야함.
                 if (mountain[temp.x][temp.y] > mountain[r][c]) {
-                    Node tmp = new Node(r, c, depth + 1, k, ft_arrayCopy(temp.visited));
+                    Node tmp = new Node(r, c, temp.depth + 1, k, ft_arrayCopy(temp.visited));
                     tmp.visited[r][c] = true;
                     q.offer(tmp);
-                } else if (mountain[temp.x][temp.y] == mountain[r][c] && k != 0 ) {
-                    Node tmp = new Node(r, c, depth + 1, 0, ft_arrayCopy(temp.visited));  // 깎고 지나갈때의 경우의 수
+                } else if (temp.k != 0 && mountain[temp.x][temp.y] > mountain[r][c] - temp.k) {
+                    Node tmp = new Node(r, c, temp.depth + 1, 0, ft_arrayCopy(temp.visited));
                     tmp.visited[r][c] = true;
-                } else if (mountain[temp.x][temp.y] < mountain[r][c] && k != 0) {
-                    if (mountain[temp.x][temp.y] >= mountain[r][c] - k) {
-                        Node tmp = new Node(r, c, depth + 1, 0, ft_arrayCopy(temp.visited));
-                        tmp.visited[r][c] = true;
-                    }
-                } else {
-                    Math.max(max, depth);
+                    q.offer(tmp);
                 }
             }
         }
