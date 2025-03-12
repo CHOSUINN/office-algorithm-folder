c767ba8105d3917a54ac6eaf45e2c39a9f83b22f
juice-in <65757661+whtndls@users.noreply.github.com>
2025-03-13 08:21:14 +0900

    update

---
+++ src/BOJ16236.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 아기 상어
+public class BOJ16236 {
+
+    static final int BABY_SHARK_SIZE_INIT = 2;
+    static final int NUMBER_OF_FISHES = 7;
+    static int numberOfGrowth = 0;
+
+    // 좌우상하 - 가까운 물고기가 여럿일경우 가장 왼쪽에 있는 물고기를 먹어야하기 때문에, 좌를 최우선으로 해주고 나머지는 그냥 설정해준다.
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int[][] map;
+    static boolean[][] visited;
+
+    // 아기 상어의 위치
+    static int[] babySharkLocation;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // 아기 상어 크기와 섭취한 물고기의 수
+        int babySharkSize = BABY_SHARK_SIZE_INIT;
+        int fishConsumed = 0;
+
+        int n = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
+
+        // 아기 상어 위치
+        babySharkLocation = new int[2];
+        // 1~6 생선들의 개수
+        int[] fishArr = new int[NUMBER_OF_FISHES];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                // 아기 상어 좌표 파악
+                if (map[i][j] == 9) {
+                    babySharkLocation[0] = i;
+                    babySharkLocation[1] = j;
+
+                    // 1~6부터 생선들의 개수를 fishArr에 저장
+                } else if (map[i][j] != 0)
+                    fishArr[map[i][j]]++;
+            }
+        }
+
+        int answer = 0;
+        boolean flag = false;
+        for (int i = 1; i < NUMBER_OF_FISHES; i++) {
+            // 섭취해야하는 생선의 개수보다 적으면 먹고 나가기
+            if (fishArr[i] < babySharkSize) {
+                flag = true;
+                break;
+            }
+
+            // 섭취해야하는 생선의 개수가 충분하면, 아기상어의 크기만큼 섭취
+            for (int j = 0; j < babySharkSize; j++)
+                visited = new boolean[n][n];
+                moveBabyShark(babySharkLocation);
+
+        }
+
+    }
+
+    // 해당 위치까지 최소시간.
+    private static void moveBabyShark(int[] babySharkLocation) {
+        Queue<int[]> q = new ArrayDeque<>();
+        int x = babySharkLocation[0];
+        int y = babySharkLocation[1];
+        q.offer(new int[]{x, y});
+
+        while (!q.isEmpty()) {
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+
+            }
+
+
+        }
+
+
+    }
+}
--- src/BOJ28066.java
+++ src/BOJ28066.java
@@ -1,41 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 // 타노스는 요세푸스가 밉다
 public class BOJ28066 {
 
-    static int answer;
-    static int n;
-    static int k;
-    static boolean[] arr;
-    static int left;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
 
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        arr = new boolean[n + 1];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for (int i = 1; i <= n; i++)
+            deque.offer(i);
 
-        for (int i = 1; i <= n; i++) {
-            arr[i] = true;
-        }
+        while (deque.size() > 1) {
 
-        left = n;
-        int index = 0;
-        while (left > 1) {
-            int ni = index + 1;
-            if (ni > n)
-                ni = 1;
-            while (true) {
-
+            if (deque.size() == 1) {
+                sb.append(deque.peek());
+                break;
             }
 
+            if (deque.size() <= k) {
+                sb.append(deque.poll());
+                break;
+            }
+
+            int temp = deque.poll();
+            deque.offer(temp);
+            for (int i = 0; i < k - 1; i++)
+                deque.poll();
+
+
         }
 
-        System.out.println(answer);
+
+        System.out.println(sb);
+
+
     }
 }
