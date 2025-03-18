8b9cf53b405095e6c431bc89e56498634d4e95d0
juice-in <65757661+whtndls@users.noreply.github.com>
2025-03-19 07:24:02 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-// 이진 검색 트리
-public class BOJ5639 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-
-    }
-}
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//// 이진 검색 트리
+//public class BOJ5639 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//
+//    }
+//}
