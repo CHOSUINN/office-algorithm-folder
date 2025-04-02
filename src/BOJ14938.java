d3140aab3582c63f384f12d41c5a82f924eebb72
juice-in <65757661+whtndls@users.noreply.github.com>
2025-04-03 08:23:13 +0900

    update

--- src/BOJ14938.java
+++ src/BOJ14938.java
@@ -1,10 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 // 서강그러운드
 public class BOJ14938 {
+
+    static final int INF = Integer.MAX_VALUE;
+    static List<Edge> edges;
+    static int[] dist;
 
     static class Edge {
         int num;
@@ -26,7 +33,14 @@ public class BOJ14938 {
         int m = Integer.parseInt(st.nextToken()); // 수색범위 
         int r = Integer.parseInt(st.nextToken()); // 길의 개수
 
-        Edge[] edges = new Edge[n + 1];
+        List<Edge>[] edges = new ArrayList[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            edges[i] = new ArrayList<>();
+        }
+
+        dist = new int[n + 1];
+        Arrays.fill(dist, INF);
+
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
 
